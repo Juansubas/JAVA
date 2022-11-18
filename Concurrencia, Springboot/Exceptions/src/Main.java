c9013ac232936632781c33f77ef1f6974cdf157b
Juansubas <juansubas@gmail.com>
2022-11-17 21:01:21 -0500

    Exception Complex

--- Concurrencia, Springboot/Exceptions/src/Main.java
+++ Concurrencia, Springboot/Exceptions/src/Main.java
@@ -1,16 +1,21 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             checkedException();
         } catch (Exception e){
             System.out.println(e.getMessage());
+            throw new Exception();
         }
+            System.out.println("Hello");
+
     }
 
+    //Checked Exceptions
     private static void checkedException() throws Exception{
             throw new Exception();
     }
 
+    //Uncheked Exceptions
     private static void exampleArithmeticException(){
         double number= 10/0;
     }
