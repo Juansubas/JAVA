7c9b07697e7e8be973788003246f807218198805
Juansubas <juansubas@gmail.com>
2022-11-21 13:48:10 -0500

    MODEL REST

--- Concurrencia, Springboot/Google Clon - BackEnd/src/main/java/com/google/google/controllers/SearchController.java
+++ Concurrencia, Springboot/Google Clon - BackEnd/src/main/java/com/google/google/controllers/SearchController.java
@@ -1,6 +1,8 @@
 package com.google.google.controllers;
 
 import com.google.google.entities.WebPage;
+import com.google.google.services.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +13,11 @@ import java.util.List;
 @RestController
 public class SearchController {
 
+    @Autowired
+    private SearchService service;
+
     @RequestMapping( value= "api/search", method = RequestMethod.GET)
-    public List<WebPage> search() {
-        List<WebPage> result = new ArrayList<>();
-        WebPage page = new WebPage();
-        page.setTitle("test");
-        page.setDescription("test");
-        result.add(page);
-        return result;
+    public List<WebPage> search(Strign textSearch) {
+        return service.search(textSearch);
     }
 }
--- Concurrencia, Springboot/Google Clon - BackEnd/src/main/java/com/google/google/services/SearchService.java
+++ Concurrencia, Springboot/Google Clon - BackEnd/src/main/java/com/google/google/services/SearchService.java
@@ -1,5 +1,20 @@
 package com.google.google.services;
 
+import com.google.google.entities.WebPage;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class SearchService {
-    
+
+    public List<WebPage> search(Strign textSearch) {
+        List<WebPage> result = new ArrayList<>();
+        WebPage page = new WebPage();
+        page.setTitle("test");
+        page.setDescription("test");
+        result.add(page);
+        return result;
+    }
 }
